64136735f2337e18671d754080645a3459c84e61
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-23 23:53:22 +0530

    feat: Add exa web search

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,54 @@
 import { anthropic } from "@ai-sdk/anthropic";
 import { generateObject, generateText, tool } from "ai";
 import "dotenv/config";
+import Exa from "exa-js";
 import { z } from "zod";
 
+const model = anthropic("claude-3-5-haiku-20241022");
+
+async function generateSearchQueries(query:string, n: number) {
+  const {object: {queries}} = await generateObject({
+    model,
+    prompt: `Generate ${n} search queries for the following query: ${query}`,
+    schema: z.object({
+      queries: z.array(z.string()).min(1).max(5)
+    })
+  })
+  return queries
+}
+
+const exa = new Exa(process.env.EXA_API_KEY)
+ 
+type SearchResult = {
+  title: string
+  url: string
+  content: string
+}
+ 
+const searchWeb = async (query: string) => {
+  const { results } = await exa.searchAndContents(query, {
+    numResults: 1,
+    livecrawl: 'always',
+  })
+  return results.map(
+    (r) =>
+      ({
+        title: r.title,
+        url: r.url,
+        content: r.text,
+      }) as SearchResult
+  )
+}
+
 const main = async () => {
+
+  // ------------------------- Deep Research ----------------------------------------
+  const prompt = "What do i need to do to get hired in top startups as Software Engineer in SF?";
+  const queries = await generateSearchQueries(prompt, 3);
+  console.log("search queries", queries);
+  const queryRes = await searchWeb(queries[1]);
+  console.log("queryRes", queryRes)
+ 
   // ------------------------- Generate Text -----------------------------------------
 
   // const result = await generateText({
@@ -52,15 +97,15 @@ const main = async () => {
 
   // -------------------------- Generate Object --------------------------------------
 
-  const result = await generateObject({
-    model: anthropic("claude-3-5-haiku-20241022"),
-    prompt:
-      "Generate 10 names for my app which is a agentic deep research tool with user experience at priority",
-    schema: z.object({
-      names: z.array(z.string().describe("Be creative, technical and clear")),
-    }),
-  });
+  // const result = await generateObject({
+  //   model: anthropic("claude-3-5-haiku-20241022"),
+  //   prompt:
+  //     "Generate 10 names for my app which is a agentic deep research tool with user experience at priority",
+  //   schema: z.object({
+  //     names: z.array(z.string().describe("Be creative, technical and clear")),
+  //   }),
+  // });
 
-  console.log("result", result.object.names);
+  // console.log("result", result.object.names);
 };
 main();
