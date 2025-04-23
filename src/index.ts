63b6b959a1dffece8bedb1aa89e761b1f0453095
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-23 23:14:51 +0530

    feat: Add example tool

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import { anthropic } from "@ai-sdk/anthropic";
 import { generateText } from "ai";
 import "dotenv/config";
+import { z } from "zod";
 
 const main = async () => {
   const result = await generateText({
     model: anthropic("claude-3-5-haiku-20241022"),
-    prompt: "Agentic Deep Research",
+    prompt: "what is 9.88 - 9.67?",
+    tools : {
+      subtractNumbers: {
+        description: "Subtract two numbers",
+        parameters: z.object({
+          num1: z.number(),
+          num2: z.number()
+        }),
+        execute: async({num1, num2}) => {
+          return num1 - num2;
+        }
+      }
+    }
+
   });
 
-  console.log("Result", result);
+  console.log("Result", result.toolResults);
 };
 main();
