17f05afe4238eb5c3009946b2c60607e311f98cb
HrushiBorhade <hrushiborhade123@gmail.com>
2025-04-23 23:26:56 +0530

    feat: Add multiple tools

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { anthropic } from "@ai-sdk/anthropic";
-import { generateText } from "ai";
+import { generateText, tool } from "ai";
 import "dotenv/config";
 import { z } from "zod";
 
 const main = async () => {
   const result = await generateText({
     model: anthropic("claude-3-5-haiku-20241022"),
-    prompt: "what is 9.88 - 9.67?",
-    maxSteps:2,
+    prompt: "what is the difference of temperature in SF and stockholm?",
+    maxSteps:4,
     tools : {
-      subtractNumbers: {
+      subtractNumbers: tool({
         description: "Subtract two numbers",
         parameters: z.object({
           num1: z.number(),
@@ -18,7 +18,28 @@ const main = async () => {
         execute: async({num1, num2}) => {
           return num1 - num2;
         }
-      }
+      }),
+      getWeather: tool({
+        description: "Get the current weather at a location",
+        parameters: z.object({
+          latitude: z.number(),
+          longitude: z.number(),
+          city: z.string(),
+        }),
+        execute: async ({ latitude, longitude, city }) => {
+          const response = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weathercode,relativehumidity_2m&timezone=auto`,
+          );
+ 
+          const weatherData = await response.json();
+          return {
+            temperature: weatherData.current.temperature_2m,
+            weatherCode: weatherData.current.weathercode,
+            humidity: weatherData.current.relativehumidity_2m,
+            city,
+          };
+        },
+      }),
     }
 
   });
